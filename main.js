5b1feaec6a4a32c9871d1f8f7b5b55d2825263c4
alljamin <aleksandr.lmn@gmail.com>
2016-04-01 11:51:20 +0300

    started to work on react-forms integration, and everything broke

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactForms = require('react-forms')
+var Schema = ReactForms.schema.Schema
+var Property = ReactForms.schema.Property
+var Form = ReactForms.Form
 
-ReactDOM.render(
-  <div>Hello, world!</div>,
-  document.getElementById('container')
-);
+// ReactDOM.render(
+//   <div>Random words</div>,
+//   document.getElementById('container')
+// );
+var schema = (
+  <Schema>
+    <Property name="randomWords" />
+    <Property name="inputWords" />
+    <Property name="statistics" /> //type="number"
+  </Schema>
+)
+React.renderComponent(
+  <Form schema={schema} />,
+  document.getElementById('container'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 module.exports = {
-  entry: './main.js',
+//   entry: '/main.js',
+  entry: 'lib/react-forms.js',
   output: {
     filename: 'bundle.js'
   },
   module: {
+      
     loaders: [
       {
+        test: /\.jsx?$/,
         loader: 'babel',
         query: {
           presets: ['react','es2015'],
         },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        // include: /.*\.js/,
+        loader: 'jsx-loader?harmony',
+        query: {
+          presets: ['react','es2015'],
+        },
+        exclude: /node_modules/
       }
     ]
   },
+  
   devServer:{
       port: 3000
   }
