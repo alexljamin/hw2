b39458b3dbafc02fe4c86a45f9b604339142237a
alljamin <aleksandr.lmn@gmail.com>
2016-04-01 16:35:38 +0300

    deleted react-forms

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactForms = require('react-forms')
-var Schema = ReactForms.schema.Schema
-var Property = ReactForms.schema.Property
-var Form = ReactForms.Form
 
-// ReactDOM.render(
-//   <div>Random words</div>,
-//   document.getElementById('container')
-// );
-var schema = (
-  <Schema>
-    <Property name="randomWords" />
-    <Property name="inputWords" />
-    <Property name="statistics" /> //type="number"
-  </Schema>
-)
-React.renderComponent(
-  <Form schema={schema} />,
-  document.getElementById('container'))
+define(["react", "react-dom", "./components/CommentBox"],
+  (React, ReactDOM, CommentBox) => {
+
+  ReactDOM.render(
+    React.createElement(CommentBox, {comments: ['Comment 1', 'Comment 2']}),
+    document.getElementById('content')
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-//   entry: '/main.js',
-  entry: 'lib/react-forms.js',
+  entry: './main.js',
   output: {
     filename: 'bundle.js'
   },
@@ -8,17 +7,7 @@ module.exports = {
       
     loaders: [
       {
-        test: /\.jsx?$/,
         loader: 'babel',
-        query: {
-          presets: ['react','es2015'],
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.jsx?$/,
-        // include: /.*\.js/,
-        loader: 'jsx-loader?harmony',
         query: {
           presets: ['react','es2015'],
         },
