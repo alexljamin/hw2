9c339324a050b798c01aa8270c019bc9142fee15
alljamin <aleksandr.lmn@gmail.com>
2016-04-02 09:21:45 +0300

    got the example from the lecture working

--- main.js
+++ main.js
@@ -1,12 +1,101 @@
-// main.js
-var React = require('react');
-var ReactDOM = require('react-dom');
+define(["react", "react-dom"],
+  (React, ReactDOM) => {
 
-define(["react", "react-dom", "./components/CommentBox"],
-  (React, ReactDOM, CommentBox) => {
+  var Comment = React.createClass({
+    render: function() {
+      return (
+        <div className="comment">
+          <h2 className="commentAuthor">
+            {this.props.author}
+          </h2>
+          {this.props.children}
+        </div>
+      );
+    }
+  });
+
+  var CommentList = React.createClass({
+    render: function() {
+      var commentNodes = this.props.data.map(function(comment) {
+        return (
+          <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+          </Comment>
+        );
+      });
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    }
+  });
+
+  var CommentForm = React.createClass({
+    getInitialState: function() {
+      return {author: '', text: ''};
+    },
+    handleAuthorChange: function(e) {
+      this.setState({author: e.target.value});
+    },
+
+    render: function() {
+      return (
+        <div>
+          <input
+            type="text"
+            placeholder="Random words"
+            value="apple, banana, truck"
+          />
+          <input
+            type="text"
+            placeholder="Insert your words"
+            value={this.state.author}
+            onChange={this.handleAuthorChange}
+          />
+        </div>
+      );
+    }
+  });
+
+  var CommentBox = React.createClass({
+    loadCommentsFromServer: function() {
+      // Lets mock the data, in reality would fetch it from server. In this case
+      // we will simply add same comment to the old data every time a pull is made from "server"
+      oldData = this.state.data
+      oldData.push({"author": "Sukram", "text": "This is niss thing"})
+      this.setState({data: oldData});
+    },
+
+    getInitialState: function() {
+      return {data: []};
+    },
+
+    componentDidMount: function() {
+      this.loadCommentsFromServer();
+      setInterval(this.loadCommentsFromServer, 10000);
+    },
+
+    handleCommentSubmit: function(comment) {
+      // Mock out server submitting and simply push the comment to existing data
+      oldData = this.state.data
+      oldData.push(comment)
+      this.setState({data: oldData});
+    },
+
+    render: function() {
+      return (
+        <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentList data={this.state.data}/>
+          <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+        </div>
+      );
+    }
+  });
 
   ReactDOM.render(
-    React.createElement(CommentBox, {comments: ['Comment 1', 'Comment 2']}),
+    <CommentBox />,
     document.getElementById('content')
   );
 });
