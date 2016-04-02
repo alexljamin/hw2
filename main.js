674e94a61e04513794e7552c674420b278c0f8ab
alljamin <aleksandr.lmn@gmail.com>
2016-04-02 18:20:35 +0300

    solution with bootstrap

--- main.js
+++ main.js
@@ -1,101 +1,49 @@
 define(["react", "react-dom"],
   (React, ReactDOM) => {
 
-  var Comment = React.createClass({
-    render: function() {
-      return (
-        <div className="comment">
-          <h2 className="commentAuthor">
-            {this.props.author}
-          </h2>
-          {this.props.children}
-        </div>
-      );
-    }
-  });
+    var RandomWords = React.createClass({
+        render:function() {
+            return (
+                <input className="form-control" id="disabledInput" type="text" value="apple, banana, truck" disabled/>
+            );
+        }
+    });
+  
 
-  var CommentList = React.createClass({
-    render: function() {
-      var commentNodes = this.props.data.map(function(comment) {
-        return (
-          <Comment author={comment.author} key={comment.id}>
-            {comment.text}
-          </Comment>
-        );
-      });
-      return (
-        <div className="commentList">
-          {commentNodes}
-        </div>
-      );
-    }
-  });
+    var UserInput = React.createClass({
+        render:function() {
+            return (
+                <input className="form-control" type="text" placeholder="Insert your words here" autoFocus/>
+            );
+        }
+    });
+  
+    var Statistics = React.createClass({
+        render:function() {
+            return (
+                <input className="form-control" id="disabledInput" type="text" value="20 word/min" disabled/>
+            );
+        }
+    });
 
-  var CommentForm = React.createClass({
-    getInitialState: function() {
-      return {author: '', text: ''};
-    },
-    handleAuthorChange: function(e) {
-      this.setState({author: e.target.value});
-    },
-
-    render: function() {
-      return (
-        <div>
-          <input
-            type="text"
-            placeholder="Random words"
-            value="apple, banana, truck"
-          />
-          <input
-            type="text"
-            placeholder="Insert your words"
-            value={this.state.author}
-            onChange={this.handleAuthorChange}
-          />
-        </div>
-      );
-    }
-  });
-
-  var CommentBox = React.createClass({
-    loadCommentsFromServer: function() {
-      // Lets mock the data, in reality would fetch it from server. In this case
-      // we will simply add same comment to the old data every time a pull is made from "server"
-      oldData = this.state.data
-      oldData.push({"author": "Sukram", "text": "This is niss thing"})
-      this.setState({data: oldData});
-    },
-
-    getInitialState: function() {
-      return {data: []};
-    },
-
-    componentDidMount: function() {
-      this.loadCommentsFromServer();
-      setInterval(this.loadCommentsFromServer, 10000);
-    },
-
-    handleCommentSubmit: function(comment) {
-      // Mock out server submitting and simply push the comment to existing data
-      oldData = this.state.data
-      oldData.push(comment)
-      this.setState({data: oldData});
-    },
-
-    render: function() {
-      return (
-        <div className="commentBox">
-          <h1>Comments</h1>
-          <CommentList data={this.state.data}/>
-          <CommentForm onCommentSubmit={this.handleCommentSubmit} />
-        </div>
-      );
-    }
-  });
-
-  ReactDOM.render(
-    <CommentBox />,
-    document.getElementById('content')
-  );
+    var App = React.createClass({
+        render:function() {
+            return(
+                <form>
+                    <h3>Random Words</h3>
+                    <RandomWords />
+                    <h3>User input</h3>
+                    <UserInput />
+                    <h3>Statistics</h3>
+                    <Statistics />
+                </form> 
+            );
+        }
+    });
+  
+    ReactDOM.render(
+        // <CommentBox />,
+        <App />,
+        document.getElementById('container')
+    );
 });
